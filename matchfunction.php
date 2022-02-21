b67ee4fc66dbe09c5d37a32aadf488c57025221d
Amirul Naim <amirulnaim.ahmadnazri@gmail.com>
2022-02-21 17:56:26 +0800

    Update with latest commit

--- matchfunction.php
+++ matchfunction.php
@@ -12,11 +12,7 @@ $result = json_decode($user_data,true);
     $sql = "UPDATE users SET matchrequest=1,matchrequestfrom='$requestfrom'  WHERE user_name='$user_name'";
     $result = $con->query($sql);
 
-<<<<<<< HEAD
     echo "<a href='index.php'>Main Menu</a>";
-=======
-    echo "success";
->>>>>>> 6c443efa88151c121f8c96557eac51b1cd286235
 
 
 
