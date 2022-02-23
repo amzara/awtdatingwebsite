56e003b081233c8fea73088b5eacad9272098037
amzara <99461764+amzara@users.noreply.github.com>
2022-02-23 20:34:40 +0800

    m

--- login_sample_db.sql
+++ login_sample_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 20, 2022 at 06:19 AM
+-- Generation Time: Feb 23, 2022 at 01:34 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.3.33
 
@@ -32,22 +32,58 @@ CREATE TABLE `users` (
   `user_id` bigint(20) NOT NULL,
   `user_name` varchar(20) NOT NULL,
   `password` varchar(20) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `email` varchar(100) NOT NULL,
   `matching` int(1) NOT NULL,
   `matchrequest` int(100) NOT NULL,
   `matchrequestfrom` varchar(100) NOT NULL,
-  `currentmatch` int(11) NOT NULL
+  `currentmatch` varchar(11) NOT NULL,
+  `daterequest` varchar(100) NOT NULL,
+  `daterequestfrom` varchar(100) NOT NULL,
+  `currentdate` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `user_id`, `user_name`, `password`, `date`, `matching`, `matchrequest`, `matchrequestfrom`, `currentmatch`) VALUES
-(22, 6715142, 'mirai1245', 'abcd', '2022-02-20 00:05:52', 1, 0, '', 0),
-(23, 131785818, 'bobotaro243', 'abc', '2022-02-20 00:46:33', 1, 0, '', 0),
-(24, 686220, 'amzar', '12345', '2022-02-20 02:48:55', 0, 0, '', 0),
-(25, 19242, 'venomon', '1234', '2022-02-20 05:14:20', 1, 0, '', 0);
+INSERT INTO `users` (`id`, `user_id`, `user_name`, `password`, `email`, `matching`, `matchrequest`, `matchrequestfrom`, `currentmatch`, `daterequest`, `daterequestfrom`, `currentdate`) VALUES
+(22, 6715142, 'mirai1245', 'abcd', '', 2, 0, '', 'venomon', '1', 'venomon', ''),
+(23, 131785818, 'bobotaro243', 'abc', '', 2, 0, '', 'abu', '', '', ''),
+(24, 686220, 'amzar', '12345', '', 0, 0, '', '0', '', '', ''),
+(25, 19242, 'venomon', '1234', '', 2, 0, '', 'mirai1245', '', '', ''),
+(27, 487770, 'haru', '123', '', 2, 0, '', 'boboi', '1', 'boboi', ''),
+(28, 49036346, 'abu', '123', '', 2, 0, '', 'bobotaro243', '1', 'bobotaro243', ''),
+(29, 748057, 'swansong', '1234', '', 2, 0, '', '', '', '', ''),
+(30, 40573995, 'maria', '1234', 'maria1234@gmail.com', 2, 0, '', 'peien', '', '', ''),
+(31, 413770, 'peien', '1234', 'peienxoxo@gmail.com', 2, 0, '', 'maria', '1', 'maria', ''),
+(37, 2648, 'parry', '123', '123', 2, 0, '', 'barry', '', '', ''),
+(38, 5812034, 'barry', '123', '123', 2, 0, '', 'parry', '1', 'parry', ''),
+(39, 4573, 'palinda', 'palindamon', '1234', 2, 0, '', 'mario', '', '', ''),
+(40, 877754756, 'mario', '123', 'mario123', 2, 0, '', 'palinda', '1', 'palinda', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usersdate`
+--
+
+CREATE TABLE `usersdate` (
+  `user_name` varchar(100) NOT NULL,
+  `date` varchar(100) NOT NULL,
+  `time` varchar(100) NOT NULL,
+  `venue` varchar(100) NOT NULL,
+  `datemessage` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `usersdate`
+--
+
+INSERT INTO `usersdate` (`user_name`, `date`, `time`, `venue`, `datemessage`) VALUES
+('parry', '', '', '', ''),
+('barry', '2022-12-15', '21:32', 'house ', 'hello'),
+('palinda', '', '', '', ''),
+('mario', '2022-02-25', '19:45', 'kota raya', 'hallo');
 
 -- --------------------------------------------------------
 
@@ -63,20 +99,36 @@ CREATE TABLE `usersinfo` (
   `location` varchar(100) NOT NULL,
   `language` varchar(100) NOT NULL,
   `height` int(100) NOT NULL,
-  `matching` int(11) DEFAULT NULL,
-  `matchrequest` varchar(100) NOT NULL,
-  `currentmatch` varchar(100) NOT NULL
+  `matching` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `usersinfo`
 --
 
-INSERT INTO `usersinfo` (`user_id`, `user_name`, `age`, `sex`, `location`, `language`, `height`, `matching`, `matchrequest`, `currentmatch`) VALUES
-(19242, 'venomon', 15, 'male', 'Afganistan', '', 0, 1, '', ''),
-(686220, 'amzar', 0, '', '', '', 0, NULL, '', ''),
-(6715142, 'mirai1245', 45, 'male', 'Malaysia', '', 0, 1, '', ''),
-(131785818, 'bobotaro243', 25, 'male', 'Croatia', '', 0, 1, '', '');
+INSERT INTO `usersinfo` (`user_id`, `user_name`, `age`, `sex`, `location`, `language`, `height`, `matching`) VALUES
+(0, '', 0, '', '', '', 0, NULL),
+(2648, 'parry', 25, 'male', 'Afganistan', '', 0, 2),
+(4573, 'palinda', 0, 'male', 'Afganistan', '', 0, 2),
+(9855, 'bbbbbbbb', 0, '', '', '', 0, NULL),
+(19242, 'venomon', 25, 'male', 'Afganistan', '', 0, 2),
+(98852, 'boboi', 1, 'male', 'Afganistan', '', 0, 2),
+(198663, 'bropro', 0, '', '', '', 0, NULL),
+(413770, 'peien', 0, '', '', '', 0, 2),
+(487770, 'haru', 15, 'male', 'Afganistan', '', 0, 2),
+(581483, 'amzar', 0, '', '', '', 0, NULL),
+(686220, 'amzar', 99, 'male', 'Angola', '', 0, 0),
+(748057, 'swansong', 27, 'male', 'Afganistan', '', 0, 2),
+(5812034, 'barry', 0, '', '', '', 0, 2),
+(6715142, 'mirai1245', 12, 'male', 'Algeria', '', 0, 2),
+(11958250, 'amzara1995', 0, '', '', '', 0, NULL),
+(28541387, 'amzar', 0, '', '', '', 0, NULL),
+(40573995, 'maria', 25, 'male', 'Afganistan', '', 0, 2),
+(49036346, 'abu', 21, 'male', 'Kuwait', '', 0, 2),
+(131785818, 'bobotaro243', 25, 'male', 'Croatia', '', 0, 2),
+(144064790, 'mirana', 0, '', '', '', 0, NULL),
+(877754756, 'mario', 0, '', '', '', 0, 2),
+(2147483647, 'koboldman', 0, '', '', '', 0, NULL);
 
 --
 -- Indexes for dumped tables
@@ -90,11 +142,21 @@ ALTER TABLE `users`
   ADD KEY `user_id` (`user_id`),
   ADD KEY `user_name` (`user_name`),
   ADD KEY `password` (`password`),
-  ADD KEY `date` (`date`),
   ADD KEY `matchup` (`matching`),
   ADD KEY `matchrequest` (`matchrequest`),
   ADD KEY `matchrequestfrom` (`matchrequestfrom`),
-  ADD KEY `currentmatch` (`currentmatch`);
+  ADD KEY `currentmatch` (`currentmatch`),
+  ADD KEY `email` (`email`);
+
+--
+-- Indexes for table `usersdate`
+--
+ALTER TABLE `usersdate`
+  ADD KEY `user_name` (`user_name`),
+  ADD KEY `date` (`date`),
+  ADD KEY `time` (`time`),
+  ADD KEY `venue` (`venue`),
+  ADD KEY `datemessage` (`datemessage`);
 
 --
 -- Indexes for table `usersinfo`
@@ -108,9 +170,7 @@ ALTER TABLE `usersinfo`
   ADD KEY `height` (`height`),
   ADD KEY `user_name` (`user_name`) USING BTREE,
   ADD KEY `user_name_2` (`user_name`),
-  ADD KEY `matching` (`matching`),
-  ADD KEY `matchrequest` (`matchrequest`),
-  ADD KEY `currentmatch` (`currentmatch`);
+  ADD KEY `matching` (`matching`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -120,7 +180,7 @@ ALTER TABLE `usersinfo`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
