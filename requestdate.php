0f6b5e8e98ad6f0dd711ad5e1d1330ccce60c41c
Amirul Naim <amirulnaim.ahmadnazri@gmail.com>
2022-02-23 20:46:32 +0800

    fix request date with isset

--- requestdate.php
+++ requestdate.php
@@ -14,19 +14,22 @@ $target=$result['currentmatch'];
 <body>
 
 <form method="POST">
-Date: <input type="date" name="date" min="2022-01-01" max="2023-01-01"><br>
-Time: <input type="time" name="time"><br>
-Venue:<input type="text" name="venue"><br>
-Drop a Message Here:<br><input type="text" name="datemessage"><br>
+Date: <input type="date" required name="date" min="2022-01-01" max="2023-01-01"><br>
+Time: <input type="time" required name="time"><br>
+Venue:<input type="text" required name="venue"><br>
+Drop a Message Here:<br><input type="text" required name="datemessage"><br>
 <input type="submit">
 </form>
 
 <?php 
 if($_SERVER['REQUEST_METHOD'] == "POST"){
-$date=$_POST['date'];
-$time=$_POST['time'];
-$venue=$_POST['venue'];
-$datemessage=$_POST['datemessage'];
+
+
+
+$date=isset($_POST['date']);
+$time=isset($_POST['time']);
+$venue=isset($_POST['venue']);
+$datemessage=isset($_POST['datemessage']);
 
 echo $date;
 echo $time;
@@ -40,13 +43,6 @@ $query = "UPDATE users SET daterequest=1,daterequestfrom='$me' WHERE user_name='
 mysqli_query($con, $query);
 $query2= "UPDATE usersdate SET date='$date',time='$time',venue='$venue',datemessage='$datemessage' WHERE user_name='$target'";
 mysqli_query($con, $query2);
-
-
-
-
-
-
-
 
 }
 ?>
